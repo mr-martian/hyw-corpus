78cc6582c7f22be2c16d6941070c8484248cb4fc
Daniel Swanson <popcorn.tomato.dude@gmail.com>
2021-10-23 10:45:28 -0400

    Bible

---
+++ bible-scrape.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+import requests
+from html.parser import HTMLParser
+from html.entities import name2codepoint
+
+writing = False
+out = None
+
+class MyHTMLParser(HTMLParser):
+    def handle_starttag(self, tag, attrs):
+        global writing, out
+        if tag == 'div':
+            if dict(attrs).get('class') in ['usfm_s1', 'usfm_p']:
+                writing = True
+            else:
+                writing = False
+        elif tag == 'b' and writing:
+            out.write('\n')
+        elif tag == 'title':
+            writing = True
+
+    def handle_endtag(self, tag):
+        global writing
+        if tag == 'title' or tag == 'div':
+            writing = False
+
+    def handle_data(self, data):
+        global writing, out
+        if writing:
+            if not out:
+                out = open(data + '.txt', 'w')
+            else:
+                out.write(data)
+
+with open('Bible_links.txt') as fin:
+    for line in fin:
+        parser = MyHTMLParser()
+        r = requests.get(line.strip())
+        parser.feed(r.text)
+        out.close()
+        out = None
