b531f9c9fa97b417bda971e3f7e462efbc8c0392
Daniel Swanson <awesomeevildudes@gmail.com>
2022-05-03 23:51:51 -0400

    scrape author info for kantsassar (probably)

--- crawlers/crawlers/spiders/kantsasar.py
+++ crawlers/crawlers/spiders/kantsasar.py
@@ -8,6 +8,12 @@ class kantsasar_spider(scrapy.Spider):
 
     def parse(self, response):
         for block in response.css('div.mg-blog-post-box'):
+            author = 'unknown'
+            for s_ in block.css('article div[style="text-align: right;"] strong::text').getall():
+                s = s_.strip()
+                if not s:
+                    continue
+                author = s # give the last one on the page
             ls = []
             for s_ in block.css('article *::text').getall():
                 s = s_.strip()
@@ -17,6 +23,7 @@ class kantsasar_spider(scrapy.Spider):
                     ls.append(s)
             yield {
                 'title': (block.css('h1 a::text').get() or '').strip(),
+                'author': author,
                 'date': (block.css('span.mg-blog-date::text').get() or '').strip(),
                 'body': '\n'.join(ls)
             }
